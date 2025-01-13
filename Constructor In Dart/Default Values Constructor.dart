e2227b5c807147901184963a305da19db8631448
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-01-13 12:00:07 +0600

    dafault values constructor

---
+++ Constructor In Dart/Default Values Constructor.dart
@@ -0,0 +1,16 @@
+class Table{
+  String? name;
+  String? color;
+
+  Table({this.name = "Table1", this.color = "White"});
+
+  void display(){
+    print("Name : ${this.name}");
+    print("Color : ${this.color}");
+  }
+}
+
+void main(){
+  Table table = Table();
+  table.display();
+}
